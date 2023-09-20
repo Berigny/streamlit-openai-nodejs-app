e07aedc208cad6ec4acc3c0f4b829f0211ba7110
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-20 04:43:15 +0000

    ANother

--- python-app/main.py
+++ python-app/main.py
@@ -6,15 +6,24 @@ from docx import Document
 from transformers import pipeline
 import torch
 from pymongo import MongoClient
-import os
 
-# Retrieve the OpenAI API key from Streamlit's secrets
-openai_api_key = st.secrets["OPENAI_API_KEY"]
-
-# Retrieve MongoDB credentials from Streamlit's secrets
+# Retrieve the MongoDB credentials from Streamlit's secrets
 mongo_uri = st.secrets["MONGO_URI"]
 mongo_user = st.secrets["MONGO_USER"]
 mongo_pass = st.secrets["MONGO_PASS"]
+
+# Connect to MongoDB using the credentials
+mongo_uri_with_auth = f"mongodb+srv://{mongo_user}:{mongo_pass}@{mongo_uri}/?retryWrites=true&w=majority"
+mongo_client = MongoClient(mongo_uri_with_auth)
+
+# Specify the database and collection after connecting
+database_name = "streamlit-openai-nodejs-app"  # Replace with your actual database name
+collection_name = "streamlit"  # Replace with your actual collection name
+db = mongo_client[database_name]
+collection = db[collection_name]
+
+# Retrieve the OpenAI API key from Streamlit's secrets
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 # Streamlit setup
 st.title('OpenAI Chat')
@@ -25,23 +34,68 @@ try:
 except KeyError:
     st.sidebar.write("You haven't set up your OpenAI API key yet.")
 
-# Function to connect to MongoDB
-def connect_to_mongodb():
-    # Build the MongoDB URI with credentials
-    mongo_uri_with_auth = f"mongodb+srv://{mongo_user}:{mongo_pass}@{mongo_uri}/?retryWrites=true&w=majority"
+# Print the PyTorch version to the console
+st.write(f'PyTorch version: {torch.__version__}')
 
-    # Connect to MongoDB
-    mongo_client = MongoClient(mongo_uri_with_auth)
-    return mongo_client
+# Set up the QA pipeline with a specific model and tokenizer
+qa_pipeline = pipeline("question-answering", model="distilbert-base-cased-distilled-squad", tokenizer="distilbert-base-cased-distilled-squad")
 
-# Connect to MongoDB
-mongo_client = connect_to_mongodb()
+# File upload logic...
+uploaded_file = st.file_uploader("Choose a file", type=['txt', 'pdf', 'docx'])
 
-# Database and collection setup
-database_name = "streamlit-openai-nodejs-app"  # Replace with your database name
-collection_name = "streamlit"  # Replace with your collection name
+if uploaded_file:
+    file_extension = Path(uploaded_file.name).suffix
 
-db = mongo_client[database_name]
-collection = db[collection_name]
+    # Read the contents based on file type
+    try:
+        if file_extension == '.txt':
+            file_contents = uploaded_file.read().decode("utf-8")
+        elif file_extension == '.pdf':
+            pdf_reader = PdfFileReader(uploaded_file)
+            file_contents = ''
+            for page in range(pdf_reader.getNumPages()):
+                file_contents += pdf_reader.getPage(page).extract_text()
+        elif file_extension == '.docx':
+            doc = Document(uploaded_file)
+            file_contents = ' '.join([p.text for p in doc.paragraphs])
+        else:
+            st.error("Unsupported file type")
+            file_contents = ''
+    except Exception as e:
+        st.error(f"Could not read file: {e}")
+        file_contents = ''
+else:
+    file_contents = ''
 
-# Rest of your code...
+# Define a maximum allowed number of tokens. Adjust as necessary.
+MAX_TOKENS = 4000  # Adjust this value based on the model's maximum token limit and your specific requirements
+
+def get_openai_response(message):
+    # Check the number of tokens in the message
+    if len(message.split()) > MAX_TOKENS:
+        return f"Input is too long. Maximum allowed tokens is {MAX_TOKENS}."
+
+    # Get a response from the OpenAI API
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ]
+    )
+    message_content = response['choices'][0]['message']['content']
+    return message_content
+
+# Input field for the user to input their query or chat
+user_input = st.text_input("You: ", file_contents)
+
+if user_input:
+    if len(file_contents) > 0:
+        try:
+            answer = qa_pipeline(question=user_input, context=file_contents, max_length=512, truncation=True)
+            st.write(f'Answer: {answer["answer"]}')
+        except Exception as e:
+            st.write(f'An error occurred: {str(e)}')
+    else:
+        # If no file is uploaded, use the OpenAI API to respond to the chat message
+        st.write(f'Assistant: {get_openai_response(user_input)}')
